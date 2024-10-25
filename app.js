52eb0877ca3fef273b90aeef20fbd40003f207f7
BayuSuryadinata <bayusurya380@gmail.com>
2024-10-25 11:19:56 +0700

    t

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 function main() {
-  
-
-
+    const hasilDadu = [1, 5, 2, 6, 2, 3]
+    let kemunculanGanjil = 0
+    let kemunculanGenap = 0
+    for (let i = 0; i < 6; i++){
+      if((hasilDadu[i] % 2) != 0){
+          kemunculanGanjil += 1
+      }else if((hasilDadu[i]%2) == 0){
+          kemunculanGenap += 1
+      }
+    }
+    console.log(`Raka mendapatkan angka ganjil sebanyaki ${kemunculanGanjil} kali dan angka genap sebanyak ${kemunculanGenap}`)
 }
+main()
 
 module.exports = main
